ca492a48ae2528a7081c5d4660457b59480a50a7
Lorenzo Zubani <lorenzozubani1999@gmail.com>
2022-02-03 15:35:56 +0100

    Create tel.ts

---
+++ tel.ts
@@ -0,0 +1,71 @@
+abstract class Smartphone {
+    numberCarica: number; //quantità euro disponibile per le chiamate
+    numeroChiamate: number; //numero chiamate del cellulare
+    unaRicarica:number; // numero ricarca 
+    minutiDurata:number; // minuti della chiamata
+    constructor(numberCarica: number, numeroChiamate: number) {
+        this.numberCarica = numberCarica;
+        this.numeroChiamate = numeroChiamate;
+    }
+
+    //void
+     ricarica (unaRicarica:number):void {
+         console.log("Ricarica :" + unaRicarica);
+         console.log("Saldo disponibile : " + this.numberCarica);
+         console.log("Saldo aggiornato: " + (this.numberCarica + unaRicarica));
+         this.numberCarica = this.numberCarica + unaRicarica;
+    }
+
+    chiamata(minutiDurata: number):void {
+        console.log("Minuti : " + minutiDurata);
+        this.numeroChiamate++;
+        if(this.numberCarica < (0.20 * minutiDurata)){
+            console.log("Credito non sufficente effettua una ricarica");
+        }
+        else {
+        this.numberCarica = this.numberCarica - (0.20 * minutiDurata);  
+        }
+    }
+
+     numero404():number {
+        return this.numberCarica;
+    }
+
+     getNumeroChiamate():number {
+        return this.numeroChiamate;
+    }
+
+    azzeraChiamate():void {
+        this.numeroChiamate = 0;
+    }
+}
+
+class FirstUser extends Smartphone {
+    numberCarica: number;
+    numeroChiamate: number;
+    constructor(numberCarica: number, numeroChiamate: number) {
+        super(numberCarica,numeroChiamate);
+    }
+}
+
+class SecondoUser extends Smartphone {
+    numberCarica: number;
+    numeroChiamate: number;
+    constructor(numberCarica: number, numeroChiamate: number) {
+        super(numberCarica,numeroChiamate);
+    }
+}
+
+
+class thirdUser extends Smartphone {
+    numberCarica: number;
+    numeroChiamate: number;
+    constructor(numberCarica: number, numeroChiamate: number) {
+        super(numberCarica,numeroChiamate);
+    }
+}
+
+
+let utente = new FirstUser(20,2);
+let utente2 = new SecondoUser(30,1);
+let utente3= new thirdUser(60,8);
